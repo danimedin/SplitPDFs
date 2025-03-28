2fe791907d8e77fe38484193fb1ab1a8ce9dcfa8
Daniel Medina Méndez <danielmedinamendez@gmail.com>
2025-03-28 13:29:51 +0100

    Generados los archivos de ejemplo

--- Principal.py
+++ Principal.py
@@ -1,34 +1,41 @@
 from pathlib import Path
 from pypdf import PdfReader, PdfWriter
 import csv
+import json
+
+def main() -> None:
+    # Leer el archivo JSON
+    with open('vars.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    pdf_path=(data["rutaEscaneados"])
 
 
-pdf_path=(Path.home()
-          / "Proxectos-Python"
-          / "Divide-Escaneo"
-          / "CA1.10 - Con Internet - Escaneados.pdf")
+    destino="PDFS/" + data["nombreArchivoExamen"]
 
-destino="PDFS/" + "CA1.10 - Con Internet - "
-
-input_pdf = PdfReader(pdf_path)
-
-#with open ("Alumnado-Escaneo.csv", newline='') as csvfile:
-with open ("Alumnado-Escaneo-FH.csv", newline='') as csvfile:
-    reader=csv.DictReader(csvfile)
-    for row in reader:
-        arquivo=row['Apelidos'] + ", " + row['Nome'] + ".pdf"
-        output_pdf = PdfWriter()
-        comienzo=int(row['Primera'])-1
-        fin=int(row['Última'])
-        indice = 0
-        for page in input_pdf.pages[comienzo:fin]:
-            if row['Rotar'] == "Non":
-                output_pdf.add_page(page)
-            else:
-                rotacion=row['Grados'].split(';')[indice]
-
-                output_pdf.add_page(page.rotate(int(rotacion)))
-            indice+=1
+    input_pdf = PdfReader(pdf_path)
 
 
-        output_pdf.write(destino  +  arquivo)
+    with open (data["rutaArchivoCSVAlumando"], newline='') as csvfile:
+        reader=csv.DictReader(csvfile)
+        for row in reader:
+            arquivo=row['Apelidos'] + ", " + row['Nome'] + ".pdf"
+            output_pdf = PdfWriter()
+            comienzo=int(row['Primera'])-1
+            fin=int(row['Última'])
+            indice = 0
+            for page in input_pdf.pages[comienzo:fin]:
+                if row['Rotar'] == "Non":
+                    output_pdf.add_page(page)
+                else:
+                    rotacion=row['Grados'].split(';')[indice]
+
+                    output_pdf.add_page(page.rotate(int(rotacion)))
+                indice+=1
+
+
+            output_pdf.write(destino  +  arquivo)
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # SplitPDFs
-Progrma en Python que divide un archivo pdf basandose en una hora csv. 
+Programa en Python que divide un archivo pdf basandose en una hoja csv.
+
+Está pensado para tener escanear todos los exámenes después de corregirlos y tener un archivo PDF con ellos.
+
+Con la ayuda de una hoja csv con la lista de alumnos, la página del PDF de cada examen y si tiene que rotarlos, genera un archivo PDF con el examen de cada alumno.
+
+
+
